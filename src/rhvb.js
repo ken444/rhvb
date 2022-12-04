b9354af471fc5b5eab035d8a6e47dfbaeeb8e505
ken444 <kenc@princip.com>
2022-12-03 19:39:37 -0500

    today

--- src/rhvb.js
+++ src/rhvb.js
@@ -1,3 +1,15 @@
-export function sayHi(user) {
-  alert(`Hello, ${user}!`);
+import { html } from 'https://unpkg.com/lit?module';
+import { component, useState } from 'https://unpkg.com/haunted/haunted.js';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return html`
+    <div part="count">${count}</div>
+    <button part="button" @click=${() => setCount(count + 1)}>
+      Increment
+    </button>
+  `;
 }
+
+customElements.define('my-counter', component(Counter));
