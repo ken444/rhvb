4bc2ecf9641fc1418f6535366b372e4606ef5d93
ken444 <kenc@princip.com>
2023-01-08 15:05:56 -0500

    jan8

--- app.js
+++ app.js
@@ -1,72 +1,11 @@
+"use strict";
+
 import { html, render } from 'https://unpkg.com/lit-html?module';
 
 import { tw } from 'https://cdn.skypack.dev/twind';
 
 //const uri = "http://localhost:3000";
 const uri = "https://node12351232153234.azurewebsites.net"
-
-
-const userAction = async () => {
-  const response = await fetch('https://principaspcoretest123452323.azurewebsites.net/api/todo', {
-
-  });
-  const myJson = await response.json(); //extract JSON from the http response
-  return JSON.stringify(myJson)
-}
-
-const userAction1 = async () => {
-  let score = {
-    date: '20220101',
-    game: '815 Philip-Lev',
-    score1: Math.floor(Math.random() * 25),
-    score2: Math.floor(Math.random() * 25)
-  };
-  const response = await fetch(uri, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(score)
-  });
-  return (await response.json()).date;
-}
-
-const userAction2 = async () => {
-  const r = await fetch(uri + "/date/20220101", {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json;charset=utf-8'
-  },
-  
-});
-  return await r.text();
-}
-
-const postScore = async (game, score1, score2) => {
-  const response = await fetch('https://principaspcoretest123452323.azurewebsites.net/api/todo', {
-
-  });
-  const myJson = await response.json(); //extract JSON from the http response
-  return JSON.stringify(myJson)
-}
-let name1 = 98;
-
-async function doit() {
-  // name1 = 8;
-  // rerender2();
-  // let url = location.hostname === "127.0.0.1" ? "http://localhost:3000/api/repos/?api-key=foo" : "56485";
-  // let response = await fetch("http://localhost:3000/api/repos/?api-key=foo");
-
-  // if (response.ok) { // if HTTP-status is 200-299
-  //   let text1 = await response.text();
-  //   document.getElementById("demo").innerHTML = text1;
-  // } else {
-  //   alert("HTTP-Error: " + response.status);
-  // }
-
-
-  document.getElementById("demo").innerHTML = await userAction1();
-}
 
 const dom1 = () => html`
 <div class=${tw`grid`}>
@@ -101,32 +40,109 @@ const dom3 = () => html`
   </div>
 `;
 
+const game = (t1, t2, id) => html`
+  <div id="${id}" @click=${() => rerender(dom(scorePage(t1,t2,id)))}
+    class=${tw`rounded-3xl bg-blue-400 p-8 m-4 grid grid-flow-col grid-cols-7 items-center`}>
+    <div class=${tw`col-span-2`}>${t1}</div>
+    <div class=${tw`justify-self-end score1`}></div>
+    <div></div>
+    <div class=${tw`col-span-2`}>${t2}</div>
+    <div class=${tw`justify-self-end score2`}></div>
+  </div>
+`;
 
+const heading = (s) => html`
+  <div
+    class=${tw`rounded-3xl bg-red-400 p-8 m-4`}>
+    <div class=${tw``}>${s}</div>
+  </div>
+`;
 
-// If you want to render based on the current values of multiple observables, you
-// want to use combineLatest(), which returns an observable that emits the latest
-// values whenever any of them change in an array.
-const dom = () => html`
-  <main class=${tw`h-screen grid auto-rows-[minmax(200px,400px)] text-1xl`}>
-    <div class=${tw`border-black border-2 grid`}>
-      <div id="demo" class=${tw`place-self-center`}>01</div>
-    </div>
-    <div class=${tw`border-black border-2 grid`}>${location.hostname}</div>
-    <div class=${tw`border-black border-2 text-red-600 text(5xl)`}>
-      ${dom1()}
-    </div>
-    ${dom2()}
-    ${dom3()}
+const scoreLine = t1 => html`
+  <div class=${tw`rounded-3xl bg-red-400 p-8 m-4 grid grid-flow-col grid-cols-3 items-center`}>
+    <div class=${tw`col-span-2`}>${t1}</div>
+    <input class=${tw`rounded-3xl`} type="number" name="score1">
+  </div>
+`;
+
+const scorePage = (t1, t2, id) => html`
+  ${scoreLine(t1)}
+  ${scoreLine(t2)}
+  <button @click=${()=>rerender(null)}>save</button>
+  </div>
+`;
+
+const schedule = 
+`
+d	20230112		
+t	8:15 OLQW		
+g	Karen	Derek	N
+g	Pao	Lev	C
+g	Philip	Brent	S
+t	8:35 OLQW		
+g	Derek	Jorge	N
+g	Scott	Pao	C
+g	Brent	Sandy	S
+t	8:55 OLQW		
+g	Jorge	Karen	N
+g	Lev	Scott	C
+g	Sandy	Philip	S
+t	9:15 OLQW		
+g	Karen	Derek	N
+g	Pao	Lev	C
+g	Philip	Brent	S
+t	9:35 OLQW		
+g	Derek	Jorge	N
+g	Scott	Pao	C
+g	Brent	Sandy	S
+t	9:55 OLQW		
+g	Jorge	Karen	N
+g	Lev	Scott	C
+g	Sandy	Philip	S
+			
+t	8:15 SP		
+g	Marcia	Aven	
+t	8:35 SP		
+g	Wayne	Marcia	
+t	8:55 SP		
+g	Aven	Wayne	
+t	9:15 SP		
+g	Marcia	Aven	
+t	9:35 SP		
+g	Wayne	Marcia	
+t	9:55 SP		
+g	Aven	Wayne	
+`
+
+let date1 = '';
+let gameIndex = 0;
+const scheduleArray = schedule.split('\n');
+const htmlArray = scheduleArray.map((element) => {
+  let r = element.split('\t');
+  switch(r[0]) {
+    case 'g':
+      return game(r[1], r[2], `game${gameIndex++}`)
+    case 't':
+      return heading(r[1]);
+    case 'd':
+      date1 = r[1]
+  }
+})
+
+const vis = (v) => v ? "" : "hidden";
+
+const mainPage = html`
+  <div class=${tw``}> ${htmlArray} </div>
+`;
+
+const dom = (page2) => html`
+  <main class=${tw`grid text-8xl`}>
+    <div class=${tw`${vis(page2)}`}> ${page2} </div>
+    <div class=${tw`${vis(!page2)}`}> ${mainPage} </div>
   </main>
 `;
 
-// And then call subscribe in order to get the TemplateResult every time it changes.
+const rerender = page => render(dom(page), document.body);
 
-let rerender2 = () => render(dom(), document.body);
+rerender(null);
 
-rerender2();
-
-document.getElementById("demo").innerHTML = "Hello JavaScript!";
-//document.getElementById("demo").innerHTML = await userAction2();
-
-//rerender2();
