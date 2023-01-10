90313c52c0c1e44901dcbe84509edc3e9557e841
ken444 <kenc@princip.com>
2023-01-10 10:47:36 -0500

    borders

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-"use strict";
-
 import { html, render } from 'https://unpkg.com/lit-html?module';
-
-
 import { tw } from 'https://cdn.skypack.dev/twind';
+import schedule from "./schedule.js";
+
+const arrayMap = (...args) => Array.prototype.map.call(...args);
 
 const scoreLine = html`
   <div class=${tw`p-4 m-4 rounded-3xl bg-red-400 grid items-center text-8xl `}>
@@ -12,64 +11,39 @@ const scoreLine = html`
   </div>
 `;
 
+const scoreLine1 = (score) => html`
+<div class=${tw`p-4 m-4 rounded-3xl bg-red-400 grid items-center text-7xl`}>
+  <div class=${tw`rounded-3xl w-1/2 place-self-end text-right mx-8`}>${score}</div>
+</div>
+`;
+
 const scorePage = html`
-  <div class=${tw`grid`}>
-    <i class=${tw`material-icons text-8xl m-4 p-4`} @click=${()=> vm.doEntry()}>close</i>
+  <div class=${tw`grid border-8 border-black h-full`}>
+    <i class=${tw`material-icons text-8xl m-4 p-4`} @click=${() => vm.doEntry()}>close</i>
     <div class=${tw`grid grid-cols-2`}>
       ${scoreLine}
       ${scoreLine}
     </div>
   
     <button tabindex="0" class=${tw`text-6xl m-8 p-8 font-semibold text-white bg-blue-500 border-b-4 border-blue-700
-      rounded-3xl shadow-md hover:bg-blue-600 hover:border-blue-800`} @click=${async ()=> saveScore()}>Save</button>
+      rounded-3xl shadow-md hover:bg-blue-600 hover:border-blue-800`} @click=${async () => vm.saveScore()}>Save</button>
+    <div class=${tw`overflow-auto border-8 border-red-800`}>
+      <div id="history"></div>
+    </div>
   </div>
-  
-  <div id="history"></div>
-    
+
 `;
 
-//const uri = "http://localhost:3000";
-const uri = "https://node12351232153234.azurewebsites.net"
-
-const  saveScore = async () => {
-
-  let score = {
-    date: vm.date,
-    game: vm.entryId,
-    scores: [vm.entryScores[0].value, vm.entryScores[1].value]
-  };
-  const response = await fetch(uri, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json;charset=utf-8' },
-    body: JSON.stringify(score)
-  });
-
-
-  vm.setScores(vm.entryId, score.scores);
-  vm.doEntry()
-}
-
-
-{/* <a class=${tw`text-gray-700 hover:text-red-500 px-2 py-1 font-bold text-white bg-red-500 rounded-full shadow-md hover:bg-red-600 hover:text-red-800`} @click=${() => {
-  // Perform cancel action here
-}}>
-  <svg class="close-icon" width="96" height="96" viewBox="0 0 96 96" fill="none" stroke="currentColor" stroke-width="8" stroke-linecap="round" stroke-linejoin="round">
-    <line x1="90" y1="6" x2="6" y2="90"></line>
-    <line x1="6" y1="6" x2="90" y2="90"></line>
-</svg> */}
-
-
-const game1 = (teams, scores) => html`
-  <div class=${tw`text-8xl rounded-2xl bg-blue-400 p-4 m-4 grid grid-flow-col grid-cols-7 items-center game`}>
-    ${team(teams[0], scores[0])}
-    <div></div>
-    ${team(teams[1], scores[1])}
+const game1 = (scores) => html`
+  <div class=${tw`grid grid-cols-2`}>
+    ${scoreLine1(scores[0])}
+    ${scoreLine1(scores[1])}
   </div>
 `;
 
-const team = (t, s) => html`
-  <div class=${tw`col-span-2 team`}>${t}</div>
-  <div class=${tw`justify-self-end score`}>${s}</div>
+const team = (t) => html`
+  <div class=${tw`col-span-2`}>${t}</div>
+  <div class=${tw`justify-self-end score`}></div>
 `;
 
 const game = (id, teams) => html`
@@ -86,55 +60,11 @@ const heading = (s) => html`
 `;
 
 const title = (s) => html`
-  <div class=${tw`rounded-2xl bg-red-400 text-6xl text-center px-8 py-2 place-self-center`}>${s}</div>
+  <div class=${tw`rounded-2xl bg-red-400 text-8xl text-center px-8 py-4 place-self-center`}>${s}</div>
 `;
 
-const schedule = 
-`
-h	Jan y 2023		
-d	20230102		
-t	8:15 OLQW		
-g	Karen	Derek	N
-g	Pao	Lev	C
-g	Philip	Brent	S
-t	8:35 OLQW		
-g	Derek	Jorge	N
-g	Scott	Pao	C
-g	Brent	Sandy	S
-t	8:55 OLQW		
-g	Jorge	Karen	N
-g	Lev	Scott	C
-g	Sandy	Philip	S
-t	9:15 OLQW		
-g	Karen	Derek	N
-g	Pao	Lev	C
-g	Philip	Brent	S
-t	9:35 OLQW		
-g	Derek	Jorge	N
-g	Scott	Pao	C
-g	Brent	Sandy	S
-t	9:55 OLQW		
-g	Jorge	Karen	N
-g	Lev	Scott	C
-g	Sandy	Philip	S
-			
-t	8:15 SP		
-g	Marcia	Aven	
-t	8:35 SP		
-g	Wayne	Marcia	
-t	8:55 SP		
-g	Aven	Wayne	
-t	9:15 SP		
-g	Marcia	Aven	
-t	9:35 SP		
-g	Wayne	Marcia	
-t	9:55 SP		
-g	Aven	Wayne	
-`
 
 let vm = {};
-
-
 
 let gameIndex = 0;
 const htmlArray = schedule.split('\n').map((element) => {
@@ -148,8 +78,8 @@ const htmlArray = schedule.split('\n').map((element) => {
       return title(entry[1]);
     case 'd':
       vm.date = entry[1];
-  }}
-)
+  }
+})
 
 const mainPage = html`
   <div class=${tw`grid text-8xl`}> ${htmlArray} </div>
@@ -157,12 +87,10 @@ const mainPage = html`
 
 const dom = html`
   <div id="mainPage" class=${tw`${''}`}> ${mainPage} </div>
-  <div id="entryPage" class=${tw`${'fixed inset-0 hidden'}`}> ${scorePage} </div>
+  <div id="entryPage" class=${tw`${'fixed inset-2 border-8 hidden'}`}> ${scorePage} </div>
 `;
 
 render(dom, document.body);
-
-const arrayMap = (...args) => Array.prototype.map.call(...args);
 
 vm.stage = -1;
 vm.entryStage = -1;
@@ -177,10 +105,10 @@ vm.setScores = (id, scores) => {
   scores.map((v,i) => vm.gameScores[id][i].innerHTML = v);
 }
 
-
 vm.doEntry = (id, teams) => {
   vm.entryId = id;
   if (id) {
+    render(html``, vm.entryHistory);
     arrayMap(vm.entryTeams, (x, i) => x.innerHTML = teams[i]);
     arrayMap(vm.entryScores, x => x.value = '');
 
@@ -189,37 +117,51 @@ vm.doEntry = (id, teams) => {
     vm.entryScores[0].focus();
 
     vm.entryStage = -1;
-    render(html``, vm.entryHistory);
-    vm.getPastScores(id);
+
+    vm.getPastScores();
   } else {
     vm.entryPage.style.display = 'none';
     vm.mainPage.style.visibility = '';
   }
 }
 
-vm.getScores = async () => {
+//const uri = "http://localhost:3000";
+const uri = "https://node12351232153234.azurewebsites.net"
 
-  const jj = `${uri}/date/'${vm.date}'/${vm.stage}`;
+vm.saveScore = async () => {
+  let score = {
+    date: vm.date,
+    game: vm.entryId,
+    scores: [vm.entryScores[0].value, vm.entryScores[1].value]
+  };
+  const response = await fetch(uri, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    body: JSON.stringify(score)
+  });
 
-  const t = await (await fetch(`${uri}/date/'${vm.date}'/${vm.stage}`)).json();
-  vm.stage = t.stage;
-
-  if (t.data) 
-  t.data.map(v=>vm.setScores(v.game, v.scores));
+  vm.setScores(vm.entryId, score.scores);
+  vm.doEntry()
 }
 
+vm.getScores = async () => {
+  const t = await (await fetch(`${uri}/date/'${vm.date}'/${vm.stage}`)).json();
+  vm.stage = t.stage;
+  t.data?.map(v=>vm.setScores(v.game, v.scores));
+}
 
-vm.getPastScores = async (id) => {
-  const t = await (await fetch(`${uri}/date/'${vm.date}'/game/'${id}'/${vm.entryStage}`)).json();
-  vm.entryStage = t.stage;
-  if (t.data) 
-    render(t.data.map(x => game1(["",""], x.scores)), vm.entryHistory);
+vm.getPastScores = async () => {
+  const id = vm.entryId;
+  if (id) {
+    const t = await (await fetch(`${uri}/date/'${vm.date}'/game/'${id}'/${vm.entryStage}`)).json();
+    vm.entryStage = t.stage;
+    if (t.data) render(t.data.map(x => game1(x.scores)), vm.entryHistory);
+  }
 }
 
 await vm.getScores();
 
 setInterval(async () => {
   await vm.getScores();
-  const id = vm.entryId;
-  if (id) await vm.getPastScores(vm.entryId);
+  await vm.getPastScores();
 }, 5000);
---
+++ schedule.js
@@ -0,0 +1,45 @@
+const schedule = 
+`
+h	Jan y 2023		
+d	20230102		
+t	8:15 OLQW		
+g	Karen	Derek	N
+g	Pao	Lev	C
+g	Philip	Brent	S
+t	8:35 OLQW		
+g	Derek	Jorge	N
+g	Scott	Pao	C
+g	Brent	Sandy	S
+t	8:55 OLQW		
+g	Jorge	Karen	N
+g	Lev	Scott	C
+g	Sandy	Philip	S
+t	9:15 OLQW		
+g	Karen	Derek	N
+g	Pao	Lev	C
+g	Philip	Brent	S
+t	9:35 OLQW		
+g	Derek	Jorge	N
+g	Scott	Pao	C
+g	Brent	Sandy	S
+t	9:55 OLQW		
+g	Jorge	Karen	N
+g	Lev	Scott	C
+g	Sandy	Philip	S
+			
+t	8:15 SP		
+g	Marcia	Aven	
+t	8:35 SP		
+g	Wayne	Marcia	
+t	8:55 SP		
+g	Aven	Wayne	
+t	9:15 SP		
+g	Marcia	Aven	
+t	9:35 SP		
+g	Wayne	Marcia	
+t	9:55 SP		
+g	Aven	Wayne	
+
+`
+
+export default schedule;
