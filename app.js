cfb1cf8b5dd1b29276177eb958e35522d96a74d9
ken444 <kenc@princip.com>
2023-01-11 07:02:40 -0500

    jan11

--- app.js
+++ app.js
@@ -2,53 +2,48 @@ import { html, render } from 'https://unpkg.com/lit-html?module';
 import { tw } from 'https://cdn.skypack.dev/twind';
 import schedule from "./schedule.js";
 
-const arrayMap = (...args) => Array.prototype.map.call(...args);
-
 const scoreLine = html`
-  <div class=${tw`p-4 m-4 rounded-3xl bg-red-400 grid items-center text-8xl `}>
-    <div class=${tw`p-4 entryTeam`}></div>
-    <input autofocus class=${tw`rounded-3xl m-4 p-0 w-1/2 place-self-end text-right focus:ring-[16px] entryScore`} type="number">
+  <div class=${tw`p-4 m-4 rounded-3xl bg-red-400 grid items-center text-8xl`}>
+    <div class=${`${tw`p-4`} entryTeam`}></div>
+    <input autofocus class=${`${tw`rounded-3xl m-4 p-0 w-1/2 place-self-end text-right focus:ring-[16px]`} entryScore`} type="number">
   </div>
 `;
 
-const scoreLine1 = (score) => html`
-<div class=${tw`p-4 m-4 rounded-3xl bg-red-400 grid items-center text-7xl`}>
+const pastScoreLine = (score) => html`
+<div class=${tw`m-4 rounded-3xl bg-red-400 grid items-center text-7xl`}>
   <div class=${tw`rounded-3xl w-1/2 place-self-end text-right mx-8`}>${score}</div>
 </div>
 `;
 
 const scorePage = html`
-  <div class=${tw`grid border-8 border-black h-full`}>
-    <i class=${tw`material-icons text-8xl m-4 p-4`} @click=${() => vm.doEntry()}>close</i>
+  <div class=${tw`grid content-start`}>
+    <i class=${`${tw`text-8xl m-4 p-4`} material-icons`} @click=${() => history.back()}>close</i>
     <div class=${tw`grid grid-cols-2`}>
       ${scoreLine}
       ${scoreLine}
     </div>
   
     <button tabindex="0" class=${tw`text-6xl m-8 p-8 font-semibold text-white bg-blue-500 border-b-4 border-blue-700
-      rounded-3xl shadow-md hover:bg-blue-600 hover:border-blue-800`} @click=${async () => vm.saveScore()}>Save</button>
-    <div class=${tw`overflow-auto border-8 border-red-800`}>
-      <div id="history"></div>
-    </div>
+                            rounded-3xl shadow-md hover:bg-blue-600 hover:border-blue-800`} @click=${async () => vm.saveScore()}>Save</button>
+    <div id="history"></div>
   </div>
 
 `;
 
-const game1 = (scores) => html`
+const pastScores = (scores) => html`
   <div class=${tw`grid grid-cols-2`}>
-    ${scoreLine1(scores[0])}
-    ${scoreLine1(scores[1])}
+    ${scores.map(v=>pastScoreLine(v))}
   </div>
 `;
 
 const team = (t) => html`
-  <div class=${tw`col-span-2`}>${t}</div>
-  <div class=${tw`justify-self-end score`}></div>
+  <div class=${`${tw`col-span-2`} team`}>${t}</div>
+  <div class=${`${tw`justify-self-end`} score`}></div>
 `;
 
 const game = (id, teams) => html`
-  <div @click=${() => vm.doEntry(id, teams)}
-    class=${tw`rounded-2xl bg-blue-400 p-4 m-4 grid grid-flow-col grid-cols-7 items-center game`}>
+  <div @click=${async () => await vm.setPage(id)}
+    class=${`${tw`rounded-2xl bg-blue-400 p-4 m-4 grid grid-flow-col grid-cols-7 items-center`} game`}>
     ${team(teams[0])}
     <div></div>
     ${team(teams[1])}
@@ -60,9 +55,8 @@ const heading = (s) => html`
 `;
 
 const title = (s) => html`
-  <div class=${tw`rounded-2xl bg-red-400 text-8xl text-center px-8 py-4 place-self-center`}>${s}</div>
+  <div class=${tw`text-4xl p-3 italic`}>${s}</div>
 `;
-
 
 let vm = {};
 
@@ -81,47 +75,65 @@ const htmlArray = schedule.split('\n').map((element) => {
   }
 })
 
-const mainPage = html`
-  <div class=${tw`grid text-8xl`}> ${htmlArray} </div>
-`;
-
 const dom = html`
-  <div id="mainPage" class=${tw`${''}`}> ${mainPage} </div>
-  <div id="entryPage" class=${tw`${'fixed inset-2 border-8 hidden'}`}> ${scorePage} </div>
+  <div id="mainPage" class=${tw`grid text-8xl`}> ${htmlArray} </div>
+  <div id="entryPage" class=${tw`${'grid hidden'}`}> ${scorePage} </div>
 `;
 
 render(dom, document.body);
 
 vm.stage = -1;
 vm.entryStage = -1;
+vm.scroll = 0;
 vm.mainPage = document.getElementById("mainPage");
 vm.entryPage = document.getElementById("entryPage");
-vm.entryTeams = vm.entryPage.getElementsByClassName('entryTeam');
-vm.entryScores = vm.entryPage.getElementsByClassName('entryScore');
+vm.entryTeams = Array.from(vm.entryPage.getElementsByClassName('entryTeam'));
+vm.entryScores = Array.from(vm.entryPage.getElementsByClassName('entryScore'));
 vm.entryHistory = document.getElementById("history");
-vm.gameScores = arrayMap(vm.mainPage.getElementsByClassName('game'), s => s.getElementsByClassName('score'));
+vm.gameScores = Array.from(vm.mainPage.getElementsByClassName('game')).map(s => { return {
+    scores: Array.from(s.getElementsByClassName('score')),
+    teams: Array.from(s.getElementsByClassName('team'))
+  }
+}
+);
 
-vm.setScores = (id, scores) => {
-  scores.map((v,i) => vm.gameScores[id][i].innerHTML = v);
+//vm.setScores = (id, scores) => scores.map((v,i) => vm.gameScores[id][i].innerHTML = v);
+
+vm.setScores = (id, scores) => vm.gameScores[id].scores.map((v,i) => v.innerHTML = scores[i]);
+
+window.addEventListener("popstate", async e => {
+  const uu = new URL(window.location.href).pathname;
+  await vm.renderContent(uu.length > 1 ? uu.substring(1) : null);
+}
+);
+
+vm.setPage = async (id) => {
+  window.history.pushState({}, "", id ? `/${id}` : '/');
+  vm.renderContent(id);
 }
 
-vm.doEntry = (id, teams) => {
+vm.renderContent = async (id) => {
   vm.entryId = id;
   if (id) {
-    render(html``, vm.entryHistory);
-    arrayMap(vm.entryTeams, (x, i) => x.innerHTML = teams[i]);
-    arrayMap(vm.entryScores, x => x.value = '');
+    try{
+      render(html`<div></div>`, document.getElementById("history"));
 
-    vm.mainPage.style.visibility = 'hidden';
-    vm.entryPage.style.display = 'block';
-    vm.entryScores[0].focus();
+      vm.entryTeams.map((x, i) => x.innerHTML = vm.gameScores[id].teams[i].innerHTML);
+      vm.entryScores.map(x => x.value = '');
 
-    vm.entryStage = -1;
+      vm.mainPage.style.display = 'none';
+      vm.entryPage.style.display = 'block';
+      vm.entryScores[0].focus();
 
-    vm.getPastScores();
-  } else {
+      vm.entryStage = -1;
+
+      await vm.getPastScores();
+    } catch {
+      const r = 0;
+    }
+    } else {
     vm.entryPage.style.display = 'none';
-    vm.mainPage.style.visibility = '';
+    vm.mainPage.style.display = '';
   }
 }
 
@@ -132,7 +144,7 @@ vm.saveScore = async () => {
   let score = {
     date: vm.date,
     game: vm.entryId,
-    scores: [vm.entryScores[0].value, vm.entryScores[1].value]
+    scores: vm.entryScores.map(v=>v.value)
   };
   const response = await fetch(uri, {
     method: 'POST',
@@ -141,27 +153,27 @@ vm.saveScore = async () => {
   });
 
   vm.setScores(vm.entryId, score.scores);
-  vm.doEntry()
+  history.back();
 }
 
 vm.getScores = async () => {
-  const t = await (await fetch(`${uri}/date/'${vm.date}'/${vm.stage}`)).json();
-  vm.stage = t.stage;
-  t.data?.map(v=>vm.setScores(v.game, v.scores));
+  const scores = await (await fetch(`${uri}/date/'${vm.date}'/${vm.stage}`)).json();
+  vm.stage = scores.stage;
+  scores.data?.map(v=>vm.setScores(v.game, v.scores));
 }
 
 vm.getPastScores = async () => {
   const id = vm.entryId;
   if (id) {
-    const t = await (await fetch(`${uri}/date/'${vm.date}'/game/'${id}'/${vm.entryStage}`)).json();
-    vm.entryStage = t.stage;
-    if (t.data) render(t.data.map(x => game1(x.scores)), vm.entryHistory);
+    const scores = await (await fetch(`${uri}/date/'${vm.date}'/game/'${id}'/${vm.entryStage}`)).json();
+    vm.entryStage = scores.stage;
+    if (scores.data) render(scores.data.map(x => pastScores(x.scores)), vm.entryHistory);
   }
 }
 
 await vm.getScores();
 
-setInterval(async () => {
+ setInterval(async () => {
   await vm.getScores();
   await vm.getPastScores();
-}, 5000);
+}, 60000);
