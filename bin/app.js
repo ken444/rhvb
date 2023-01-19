12dbd51587c082c9e8bcf87ca7caf1cb04c99674
ken444 <kenc@princip.com>
2023-01-19 14:12:58 -0500

    test 4

--- bin/app.js
+++ bin/app.js
@@ -1,15 +1,20 @@
 //import { } from '../schedules/bin/app.js';
 
 import { html, render } from 'https://unpkg.com/lit-html?module';
-import { tw } from 'https://cdn.skypack.dev/twind';
+//import { tw } from 'https://cdn.skypack.dev/twind';
 // import schedule from "./schedule.js";
 // import createvm from "./vm.js";
 
 
 
+// const dom = html`
+// <div class=${tw`mx-auto max-w-[1024px] min-w-[720px]`}>
+//   <div id="mainPage" class=${tw`grid text-7xl`}> ${"hellow"} </div>
+// </div>
+// `;
 const dom = html`
-<div class=${tw`mx-auto max-w-[1024px] min-w-[720px]`}>
-  <div id="mainPage" class=${tw`grid text-7xl`}> ${"hellow"} </div>
+<div >
+  <div id="mainPage" > ${"hellow"} </div>
 </div>
 `;
 
