669ec20749e69f948ba7a9ee0ba27229501e7859
ken444 <kenc@princip.com>
2023-02-11 23:27:33 -0500

    feb 16 added

--- schedules/src/app.js
+++ schedules/src/app.js
@@ -6,7 +6,7 @@ import createvm from "./vm.js";
 const scoreLine = html`
   <div class=${tw`p-4 m-4 rounded-3xl bg-red-400 grid items-center text-8xl`}>
     <div class=${`${tw`p-4`} entryTeam`}></div>
-    <input autofocus class=${`${tw`rounded-3xl m-4 p-0 w-1/2 place-self-end text-right focus:ring-[16px]`} entryScore`}
+    <input autofocus class=${`${tw`rounded-3xl m-4 p-0 w-1/2 place-self-end text-right focus:ring-[32px]`} entryScore`}
       type="number">
   </div>
 `;
