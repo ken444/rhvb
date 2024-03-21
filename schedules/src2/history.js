6f9c1dfd5e6cc6cb57cf602acb37c2b376e07bb5
ken444 <kenc@princip.com>
2024-03-21 15:47:00 -0400

    test

---
+++ schedules/src2/history.js
@@ -0,0 +1,29 @@
+//import { LitElement, html }  from '/node_modules/lit/html.js';
+
+//import '/node_modules/lit-html/lit-html.js';
+
+//import '/node_modules/lit-element/lit-element.js';
+
+
+//import '/node_modules/lit/index.js';
+
+
+
+// class MyElement extends LitElement {
+
+//   // Implement `render` to define a template for your element.
+//   render(){
+//     /**
+//      * Return a lit-html `TemplateResult`.
+//      *
+//      * To create a `TemplateResult`, tag a JavaScript template literal
+//      * with the `html` helper function.
+//      */
+//     return html`
+//       <div>
+//         <p>A paragraph</p>
+//       </div>
+//     `;
+//   }
+// }
+// customElements.define('my-history', MyElement);
