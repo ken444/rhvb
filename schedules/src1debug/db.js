7f962638cb4e78f5cbb5284e0cf41aae5695ecd1
ken444 <kenc@princip.com>
2025-03-13 23:32:08 -0400

    5

--- schedules/src1debug/db.js
+++ schedules/src1debug/db.js
@@ -10,35 +10,10 @@ export default function db() {
     let stage1 = -1;
     let stage2 = -1;
 
-    // return {
-
-    //     async saveScore(x) {
-    //         await fetch(`${uri}/V2`, {
-    //             method: 'POST',
-    //             headers: { 'Content-Type': 'application/json;charset=utf-8' },
-    //             body: JSON.stringify(x)
-    //         });
-    //     },
-
-    //     async getScores(date) {
-    //         const scores1 = await fetch(`${uri}/V2/date/'${date}'/${stage1}`);
-    //         const scores = await scores1.json();
-    //         stage1 = scores.stage;
-    //         return scores.data;
-    //     },
-
-    //     async getPastScores(date, id, force) {
-    //         const scores = await (await fetch(`${uri}/V2/date/'${date}'/game/'${id}'/${force ? -1 : stage2}`)).json();
-    //         stage2 = scores.stage;
-    //         return scores.data;
-    //     }
-
-    // }
-
     return {
 
         async saveScore(x) {
-            await fetch(`${uri}/V3`, {
+            await fetch(`${uri}/V2`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json;charset=utf-8' },
                 body: JSON.stringify(x)
@@ -46,17 +21,42 @@ export default function db() {
         },
 
         async getScores(date) {
-            const scores1 = await fetch(`${uri}/V3/?date=${date}&stage=${stage1}`);
+            const scores1 = await fetch(`${uri}/V2/date/'${date}'/${stage1}`);
             const scores = await scores1.json();
             stage1 = scores.stage;
             return scores.data;
         },
 
         async getPastScores(date, id, force) {
-            const scores = await (await fetch(`${uri}/V3?date=${date}&game=${id}&stage=${force ? -1 : stage2}`)).json();
+            const scores = await (await fetch(`${uri}/V2/date/'${date}'/game/'${id}'/${force ? -1 : stage2}`)).json();
             stage2 = scores.stage;
             return scores.data;
         }
 
     }
+
+    // return {
+
+    //     async saveScore(x) {
+    //         await fetch(`${uri}/V3`, {
+    //             method: 'POST',
+    //             headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    //             body: JSON.stringify(x)
+    //         });
+    //     },
+
+    //     async getScores(date) {
+    //         const scores1 = await fetch(`${uri}/V3/?date=${date}&stage=${stage1}`);
+    //         const scores = await scores1.json();
+    //         stage1 = scores.stage;
+    //         return scores.data;
+    //     },
+
+    //     async getPastScores(date, id, force) {
+    //         const scores = await (await fetch(`${uri}/V3?date=${date}&game=${id}&stage=${force ? -1 : stage2}`)).json();
+    //         stage2 = scores.stage;
+    //         return scores.data;
+    //     }
+
+    // }
 }
